89c389268ffb56af146d3c6a1ff83cd04392d97f
CGHQ\stefan.nedelcu <stefan.nedelcu@cyberghost.ro>
2018-11-07 11:39:28 +0200

    Adaugare intrebare + modificare cerinte

--- cerinte.sql
+++ cerinte.sql
@@ -1,25 +1,41 @@
--- #1 Afiseaza pentru fiecare comanda numele si email-ul cumparatorului si data comenzii
+-- #1 Afiseaza pentru fiecare comanda: id-ul comenzii, numele, prenumele si email-ul cumparatorului si data comenzii
+-- | id | first_name | last_name | email | date_order |
 
 -- #2 Afiseaza toate comenzile din 2017
+-- A Se folosi functia COUNT()
+-- | total_comenzi |
+
 
 -- #3 Afiseaza numarul total al comenzilor valide
 -- A Se folosi functia COUNT()
+-- | total_comenzi |
 
 
--- #4 Afiseaza numarul total de comenzi pe luna
+-- #4 Afiseaza numarul total de comenzi pe luna 
 -- A se folosi Group By
 -- Si functia MONTH() - returneaza luna dintr-o data pe care o dai ca argument
+
+-- | luna | nr_comenzi |
 
 
 -- #5 Sa se afiseze cate produse sunt comandate in fiecare comanda
 --  A se folosi GROUP BY si COUNT()
+-- | id | date_order |  nr_products |
 
 
 -- #6 Sa se slecteze cele mai populare 3 produse dupa numarul de comenzi
 --  A se folosi GROUP BY si COUNT()
+-- | id_product | nr_comenzi |  name |
 
 
 -- #7 Sa se afiseze TOP 10 clienti in functie de numarul de comenzi
+-- | id | first_name | last_name  | nr_orders |
 
 
 -- #8 Sa se afiseze suma totala a comenzilor valide (status=1) din anul 2018
+-- | id | order_value |
+
+-- #9 Sa se afiseze pentru un utilizator ( cautat dupa id, ex: 149) toate prousele comandate si cantitatea
+-- | name | total_quantity |
+
+
--- rezolvari.sql
+++ rezolvari.sql
@@ -1,5 +1,5 @@
 -- #1 Afiseaza pentru fiecare comanda numele si email-ul cumoaratorului si data comenzii
-SELECT ord.id, c.first_name, c.last_name,  c.email
+SELECT ord.id, c.first_name, c.last_name,  c.email, ord.date_order
 FROM `orders` as ord 
 LEFT JOIN `customers` as c ON (ord.`id_customer` = c.`id`)    
 WHERE 1
@@ -7,20 +7,20 @@ WHERE 1
 
 -- #2 Afiseaza toate comenzile din 2017
 
-SELECT COUNT(`id`)
+SELECT COUNT(`id`) as total_comenzi
 FROM `orders` 
 WHERE `date_order`>'2017-01-01' AND `date_order`<'2017-12-31'
 
 -- #3 Afiseaza numarul total al comenzilor valide
 -- A Se folosi functia COUNT()
-SELECT COUNT(`id`)
+SELECT COUNT(`id`) as total_comenzi
 FROM `orders` 
 WHERE `status`='1'
 
 -- #4 Afiseaza numarul total de comenzi pe luna
 -- A se folosi Group By
 -- Si functia MONTH() - imi returneaza luna dintr-o data
-SELECT  MONTH(date_order), COUNT(`id`) 
+SELECT  MONTH(date_order), COUNT(`id`) as nr_comenzi 
 FROM `orders` 
 WHERE `date_order`>'2018-01-01' AND `date_order`<'2018-12-31'
 GROUP BY MONTH(date_order)
@@ -41,7 +41,8 @@ ORDER BY `nr_products`  DESC
 -- Folosim GROUP BY le grupezi dupa un criteriu, in cazul asta id-ul produsului.
 -- Cu COUNT() numar cate comenzi sunt pentru fiecare produs
 -- Folosesc abrevieri pentru a usura scrierea query-ului
-SELECT op.`id_product`,COUNT(op.`id_order`) as nr_comenzi, p.`name` FROM `orders_products` as op
+SELECT op.`id_product`,COUNT(op.`id_order`) as nr_comenzi, p.`name` 
+FROM `orders_products` as op
 LEFT JOIN `products` as p ON (p.`id` = op.`id_product`)
 WHERE 1
 GROUP BY `id_product`  
@@ -71,3 +72,12 @@ WHERE ord.`date_order`>'2018-01-01' AND ord.`date_order`<'2018-12-31'
 GROUP BY ord.`id`  
 ORDER BY `ord`.`id` ASC
 
+-- #9 Sa se afiseze pentru un utilizator ( cautat dupa id, ex: 149) toate prousele comandate si cantitatea
+
+SELECT p.name, SUM(op.quantity) as total_quantity
+FROM `orders_products` AS op
+LEFT JOIN `products` as p ON (p.id = op.id_product)
+LEFT JOIN `orders` as ord ON (ord.id = op.id_order)
+LEFT JOIN `customers` as c ON (ord.id_customer = c.id)
+WHERE c.id = '149'
+GROUP BY op.id_product
